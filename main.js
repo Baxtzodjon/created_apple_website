0d06c0db88f8a70d1552ee27d9eeab6556f84d45
Baxtzodjon <rasulovbaxtzod@gmailcom>
2023-11-13 23:14:37 +0500

    added modal box

--- main.js
+++ main.js
@@ -4,40 +4,8 @@ let img = document.querySelector('#photo_macbook')
 let h1_buy = document.querySelector('#h1_buy')
 let color_name = document.querySelector('#color_name')
 
-white.onclick = () => {
-    img.setAttribute('src', './img/macbook_white.png')
 
-    if (h1_buy.innerText === "$2,599") {
-        h1_buy.innerText = "$1,999"
-    } else {
-        h1_buy.innerText === "$2,599"
-    }
-
-    if (color_name.innerText === "Space Gray") {
-        color_name.innerText = "White"
-    } else {
-        color_name.innerText === "Space Gray"
-    }
-}
-
-space_grey.onclick = () => {
-    img.setAttribute('src', './img/macbook_space_gray.png')
-
-    if (h1_buy.innerText === "$1,999") {
-        h1_buy.innerText = "$2,599"
-    } else {
-        h1_buy.innerText === "$1,999"
-    }
-
-    if (color_name.innerText === "White") {
-        color_name.innerText = "Space Gray"
-    } else {
-        color_name.innerText === "White"
-    }
-}
-
-
-/* let btns = document.querySelectorAll('.btn_blue')
+let btns = document.querySelectorAll('.btn_blue')
 
 let prev = 0
 
@@ -46,39 +14,47 @@ btns.forEach((btn, idx) => {
         btns[prev].classList.remove('active')
         btn.classList.add('active')
 
+        if (idx === 0) {
+            img.setAttribute('src', './img/macbook_white.png')
+
+            if (h1_buy.innerText === "$2,599") {
+                h1_buy.innerText = "$1,999"
+            } else {
+                h1_buy.innerText === "$2,599"
+            }
+
+            if (color_name.innerText === "Space Gray") {
+                color_name.innerText = "White"
+            } else {
+                color_name.innerText === "Space Gray"
+            }
+        } else {
+            img.setAttribute('src', './img/macbook_space_gray.png')
+
+            if (h1_buy.innerText === "$1,999") {
+                h1_buy.innerText = "$2,599"
+            } else {
+                h1_buy.innerText === "$1,999"
+            }
+
+            if (color_name.innerText === "White") {
+                color_name.innerText = "Space Gray"
+            } else {
+                color_name.innerText === "White"
+            }
+        }
+
         prev = idx
     }
-}) */
+})
 
 
+let btns_memory = document.querySelectorAll('.btn_TB_SSD')
+let price = 1999
 
-
-// let btn_price = document.querySelector('#btn_price')
-
-// btn_price.onclick = () => {
-//     if (h1_buy.innerText === "$1,999") {
-//         h1_buy.innerText = "$2,199"
-//     } else {
-//         h1_buy.innerText === "$1,999"
-//     }
-// }
-
-// let btn_price2 = document.querySelector('#btn_price2')
-
-// btn_price2.onclick = () => {
-//     if (h1_buy.innerText === "$1,999") {
-//         h1_buy.innerText = "$2,599"
-//     } else {
-//         h1_buy.innerText === "1,999"
-//     }
-// }
-
-// let btn_price3 = document.querySelector('#btn_price3')
-
-// btn_price3.onclick = () => {
-//     if (h1_buy.innerText === "$1,999") {
-//         h1_buy.innerText = "$3,599"
-//     } else {
-//         h1_buy.innerText === "1,199"
-//     }
-// }
+btns_memory.forEach(btn => {
+    let price_additional = btn.querySelector('span')?.innerHTML || "0"
+    btn.onclick = () => {
+        h1_buy.innerHTML = (+price + +price_additional).toLocaleString('en-EN')
+    }
+})
--- script.js
+++ script.js
@@ -24,3 +24,14 @@ black.onclick = () => {
     img.setAttribute('src', 'https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/iphone-15-pro-finish-select-202309-6-7inch-blacktitanium?wid=2560&hei=1440&fmt=p-jpg&qlt=80&.v=1692845694698')
     h4.innerText = "Color - Black Titanium"
 }
+
+
+
+// modal window js code
+let faqs =  document.querySelectorAll(".faq");
+
+faqs.forEach(faq => {
+    faq.onclick = () => {
+        faq.classList.toggle("active");
+    }
+})
